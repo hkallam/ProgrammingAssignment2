dac5528aaa121da4bb82a2435f92f98d1579b9e7
HimaBindu Kallam <Himabindukallam@outlook.com>
2014-08-23 12:54:02 -0700

    Adding updated code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix  takes square matrix as an argument.
+## sets the inverse of a matrix by invoking the setinverse from   cacheSolve
+## returns the list containg the subfunctions like 1.set 2.get 3.setinverse 4.getinverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL         
+  set<-function(n)
+  {  
+    x<<-n;     ## superassaingnment operator is used to asaign a value to the variable in the parent function(the one step above )
+    i<<-NULL;
+  }
+  get<-function()
+    x
+  setinverse<-function(inverse)
+  {
+    i<<-inverse
+  }
+  getinverse<-function()
+    i
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+##cacheSolve gets the inverse matrix(i.e already calculated matrix) by calling getinverse,checks if calculated or not before
+##otherwise takes the given matrix ,calculates the inverse matrix by solve() and returns it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i=x$getinverse()
+  if(!is.null(i))
+  {
+    message("got cached data")
+    return(i)
+  }
+  data=x$get()
+  inv=solve(data)
+  x$setinverse(inv)
+  inv
+  ## Return a matrix that is the inverse of 'x'
 }
